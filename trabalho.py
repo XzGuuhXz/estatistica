b427f3e2fc49100b7b68939e8e4d100bda8cc289
XzGuuhXz <gustavohenrique07101427@gamil.com>
2026-03-02 17:12:55 +0000

    update

--- trabalho.py
+++ trabalho.py
@@ -5,5 +5,8 @@ df = pdi.read_csv('fipe_2022.csv',
                   sep=',',
                   encoding='utf-8')
 
+array_yer_model = (df['year_model'].copy())
+array_avg_price_brl = (df['avg_price_brl'].copy())
+array_yer_model = (df['age_years'].copy())
 print(df.head())
 
